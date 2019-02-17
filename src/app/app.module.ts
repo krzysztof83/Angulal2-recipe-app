3fb60f9f216da9a24d86fd8517e2ac1e208ce651
krzysztof83 <k.czechowski83@gmail.com>
2019-02-17 17:04:21 +0100

    add StoreRouterConnectingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import {StoreModule} from '@ngrx/store';
 import {reducers} from './store/app.reducers';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+
 
 
 @NgModule({
@@ -30,7 +32,8 @@ import {AuthEffects} from './auth/store/auth.effects';
     AuthModule,
     CoreModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule
   ],
   providers: [
     RecipeService,
